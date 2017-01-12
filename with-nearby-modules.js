2513d4829678f2717d2ffacd238d6e6f6aa2762a
Erik Pukinskis <erik@snowedin.net>
2017-01-11 16:38:35 -0800

    Offer data to other modules who might want it

---
+++ with-nearby-modules.js
@@ -0,0 +1,80 @@
+
+module.exports = generator()
+
+function generator() {
+  var callbacksWaitingOn = {}
+  var singletons = {}
+  var callbacks = []
+  var dependencies = []
+
+  function withNearbyModules(moduleNames, callback) {
+    console.log("a callback is waiting on "+moduleNames.join(", "))
+    var i = callbacks.length
+    callbacks.push(callback)
+    dependencies.push(moduleNames)
+
+    moduleNames.forEach(function(name) {
+      if (!callbacksWaitingOn[name]) {
+        callbacksWaitingOn[name] = new Set()
+      }
+
+      callbacksWaitingOn[name].add(i)
+    })
+  }
+
+  function stringify(x) {
+    if (x && x.toString) {
+      return x.toString()
+    } else {
+      return JSON.stringify(x)
+    }
+  }
+
+  function aModuleAppeared(name, singletonGenerator) {
+
+    if (typeof singletonGenerator != "function") {
+      throw new Error("withNearbyModules.aModuleAppeared needs a name and a function that returns singletons. For the module "+stringify(name)+" you provided "+stringify(singletonGenerator)+" for the function")
+    }
+
+    console.log("A WILD MODULE APPEARED! "+name+"! Generator is "+stringify(singletonGenerator))
+
+    singletons[name] = singletonGenerator
+
+    console.log("keys on singletons include "+Object.keys(singletons))
+
+    var waiting = callbacksWaitingOn[name]
+
+    if (!waiting) { return }
+
+    waiting.forEach(tryToCall)
+
+    function tryToCall(index) {
+      var func = callbacks[index]
+      var deps = dependencies[index]
+      var args = []
+      console.log("call the callback waiting for "+deps.join(", ")+"?")
+      for(var i=0; i<deps.length; i++) {
+        var dep = deps[i]
+
+        var singletonGenerator = singletons[dep]
+        if (!singletonGenerator) {
+          console.log("checking for "+dep+" -> NOPE aborting.")
+          console.log("keys on singletons include "+Object.keys(singletons))
+          console.log("global store is", globalStore.id)
+          return
+        }
+        console.log("checking for "+dep+" X")
+
+        var singleton = singletonGenerator.call()
+        args.push(singleton)
+      }
+
+      func.apply(null, args)
+    }
+
+  }
+
+  withNearbyModules.aModuleAppeared = aModuleAppeared
+
+  return withNearbyModules
+}
